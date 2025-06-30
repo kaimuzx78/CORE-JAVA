652c9a947c7a75b62da2ad3894d64b917909501a
kaimuzx78 <kaimuddin78652@gmail.com>
2025-06-30 12:12:27 +0530

    muttu sawami

---
+++ Qspiders/src/box3/MuttuSawami.java
@@ -0,0 +1,22 @@
+package box3;
+
+public class MuttuSawami {
+	
+	static {
+		System.out.println("1 Khusbhu join Qspiders");
+	}
+	static {
+		System.out.println("2 Aadesh ka dil garden garden huwa");
+	}
+	public static void main(String[] args) {
+		System.out.println("3 Main Start");
+		
+		System.out.println("4 Main End");
+
+	}
+	static {
+		System.out.println("5 Kushbu Akle got placement");
+		System.out.println("6 Itna chubne laga hu chura toh nai :(");
+	}
+
+}
